4ea2c54c88510f83985da487e8f5163d0176f654
Soham Yogesh Yedgaonkar <sohamyedgaonkar@gmail.com>
2024-08-14 10:16:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from difflib import get_close_matches
 import requests
 from bs4 import BeautifulSoup as bs
 from transformers import pipeline
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Initialize the Flask application
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secretkey'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 # Initialize sentiment analysis pipeline
 pipe = pipeline("text-classification", model="mrm8488/distilroberta-finetuned-financial-news-sentiment-analysis")
@@ -45,15 +62,17 @@ def get_bbc_text(url: str):
     return [b.get_text() for b in body]
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
-@app.route('/process', methods=['POST'])
-def process():
+@app.route('/analyze', methods=['POST'])
+@login_required
+def analyze():
     url = request.form['link']
     
     if not url:
-        return render_template('index.html', error="URL is required")
+        return jsonify({"error": "URL is required"}), 400
     
     result = get_bbc_text(url)
     final_list = []
@@ -65,8 +84,10 @@ def process():
         
         for word in text.split():
             matcho = get_close_matches(word, cleaned_company_names, n=1, cutoff=.70)
-            if matcho and word[0] == matcho[0][0]:
-                abc.add(matcho[0])
+            if matcho:
+                if (word[0]==matcho[0][0]):
+                    abc.add(matcho[0])
+               
         
         res = pipe(text)
         
@@ -84,12 +105,47 @@ def process():
                 final_neg = [final_neg[i] + res[0]['score'] * mark for i in range(len(final_neg))]
 
     final_list = [x.replace("BJP", "ADANI Group") for x in final_list]
-    final_list = list(map(lambda x: x.replace("Windows", "Microsoft"), final_list))
-    final_list = list(map(lambda x: x.replace("Google", "Alphabet"), final_list))
-
+    final_list=list(map( lambda x: x.replace("Windows","Microsoft"),final_list))
+    final_list=list(map( lambda x: x.replace("Google","Alphabet"),final_list))
     finalpos = [final_list[i] for i in range(len(final_list)) if final_neg[i] > 0]
     finalneg = [final_list[i] for i in range(len(final_list)) if final_neg[i] <= 0]
 
     return render_template('index.html', positive_companies=finalpos, negative_companies=finalneg)
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    #checking if login is valid
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html')
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash('Account created successfully! Please login.', 'success')
+        return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
